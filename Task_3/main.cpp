1c8545eadc314540e312f9a2e34c78e7d64da7ed
Ploshka <folga12381@gmail.com>
2021-10-23 19:58:31 +1000

    Testing file

--- Task_3/main.cpp
+++ Task_3/main.cpp
@@ -22,20 +22,7 @@ int main()
         }
     }
 	std::cout << mmx << std::endl;
-	std::cout << (mmx * 2) << std::endl;
-	//*/
-	/*
-	Matrix mmx(4, 4);
-	for (int i = 0; i < mmx.getRows(); i++)
-    {
-        for (int j = 0; j < mmx.getColumns(); j++)
-        {
-            mmx[i][j] = (i + 1) * (j + 1);
-        }
-    }
-
-	Matrix mx(mmx);
-	std::cout << mmx << std::endl;
+	Matrix mx(mmx * 2);
 	std::cout << mx << std::endl;
-	*/
+	std::cout << (mx - (2 * mmx)) << std::endl;
 }
