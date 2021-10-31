1994e2e27546b8be09e0c7c3dfdb4c49c2a35ca9
Ploshka <folga12381@gmail.com>
2021-10-31 22:40:49 +1000

    Slightly rebuild the assembly of the program

---
+++ Task_4.1/Exception.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+class MatrixException
+{
+public:
+    MatrixException(std::string error);
+};
+
+class SizeException:public MatrixException
+{
+public:
+    SizeException(std::string error);
+};
+
+class IndexException:public MatrixException
+{
+public:
+    IndexException(std::string error);
+};
---
+++ Task_4.1/build.sh
@@ -0,0 +1,10 @@
+case "$1" in
+-f) Mode="File";;
+-c) Mode="Console";;
+esac
+
+cmake -S. -B Build -DMODE=${Mode}
+
+cd Build
+
+make
