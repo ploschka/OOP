5b1110eff97b890d1071daab97944874eded17e4
Ploshka <folga12381@gmail.com>
2021-10-31 17:30:39 +1000

    Changed dumb number to cool keys

--- Task_4/dinamicrun.sh
+++ Task_4/dinamicrun.sh
@@ -1,11 +1,11 @@
 cd DinamicBuild/
 
 case "$1" in
-1) mv Console/libPrint.so ./;;
-2) mv File/libPrint.so ./;;
+-c) mv Console/libPrint.so ./;;
+-f) mv File/libPrint.so ./;;
 esac
 ./main
 case "$1" in
-1) mv libPrint.so Console/;;
-2) mv libPrint.so File/;;
+-c) mv libPrint.so Console/;;
+-f) mv libPrint.so File/;;
 esac
